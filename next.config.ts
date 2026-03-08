95978d209ca3d9336fa4f43e38dbe227a368514c
mist3rth <mist3rth@users.noreply.github.com>
2026-03-08 09:57:50 +0100

    fix(deploy): simplified robust workflow and next static config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: '/Reflex.io',
-  assetPrefix: '/Reflex.io',
   images: {
     unoptimized: true, // Nécessaire pour l'export Next.js complet (SSG)
   },
